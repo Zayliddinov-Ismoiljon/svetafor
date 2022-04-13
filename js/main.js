a4ceefb4f846bd278040814e3bf2cba87669877e
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-04-13 18:51:35 +0500

    Innitial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const elRed= document.querySelector(".red");
+const elYellow= document.querySelector(".yellow");
+const elGreen= document.querySelector(".green");
+
+setInterval(()=>{
+    setTimeout(() => {
+        elRed.style.backgroundColor= "red";
+    },1000);
+
+    setTimeout(() => {
+        elRed.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },1500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },2000);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },2500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },3000);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },3500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },4000);
+
+    setTimeout(() => {
+        elGreen.style.backgroundColor= "green";
+    },4500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },6000);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },6500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },7000);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },7500);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "yellow";
+    },8000);
+
+    setTimeout(() => {
+        elYellow.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },8500);
+
+    setTimeout(() => {
+        elGreen.style.backgroundColor= "green";
+    },9000);
+
+    setTimeout(() => {
+        elGreen.style.backgroundColor= "rgba(86, 85, 85, 0.325)";
+    },9500);
+}, 9500)
